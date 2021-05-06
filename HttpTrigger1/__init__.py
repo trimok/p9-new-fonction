a9521cb60dccaa1f6fbc01c9ec9b843a958a9128
trimok <trimok@mail.fr>
2021-05-06 11:44:45 +0200

    conseil dynamique articles

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -1,31 +1,50 @@
 import logging
 import json
-
-from keras import backend
-from keras.models import load_model
-from keras.utils import Sequence
-from keras.preprocessing.image import load_img, img_to_array, array_to_img, save_img
-from keras.callbacks import *
-
-from keras import layers
-from keras import models
-from keras import Model
-from keras.layers import *
-from keras.models import *
-from keras.optimizers import *
-from keras import regularizers
+import pandas
 
 import azure.functions as func
 
-def test_keras():
-    model = Sequential()
-    model.add(Dense(12, input_dim=8, activation='relu'))
-    model.add(Dense(8, activation='relu'))
-    model.add(Dense(1, activation='sigmoid'))
 
+web_init_df = False
 
+# Initialisations : on charge les dataframe en mémoire
+def web_init(cf_recommend_filename=None, cb_recommend_filename=None):
+    global web_init_df
+    global dict_df
+    
+    if not web_init_df:
+        try:
+            df_cf_recommend = pd.read_csv(cf_recommend_filename)
+        except:
+            pass
+        try:
+            df_cb_recommend = pd.read_csv(cb_recommend_filename)
+        except:
+            pass
+        dict_df = {"cf":df_cf_recommend, "cb" :df_cb_recommend}
+        
+        web_init_df = True
+
+# Fonction pour le web
+# Récupère les articles les plus appropriés pour un utilisateur        
+def web_search_items(type_recommandation, userId, 
+                     cf_recommend_filename="data/cf_recommend.csv", 
+                     cb_recommend_filename = "data/cb_recommend.csv"):
+    global dict_df
+    
+    # Initialisations si nécessaire
+    web_init(cf_recommend_filename=cf_recommend_filename, cb_recommend_filename=cb_recommend_filename);
+    
+    # Les données correspondant au bon type de filtering
+    df = dict_df [type_recommandation]
+    
+    # Les items conseillés
+    items = df[df.user == userId].sort_values(by=['rank'])["item"].tolist()
+    
+    return items
+
+# Main
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    test_keras()
     logging.info('Python HTTP trigger function processed a request.')
 
     userId = req.params.get('userId')
@@ -40,6 +59,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             userId = req_body.get('userId')
 
     if userId:
-        return func.HttpResponse(json.dumps([2137,2662,4243]), mimetype="application/json")        
+        list_item = web_search_items("cb", userId)
+        return func.HttpResponse(json.dumps(list_item), mimetype="application/json")        
     else:
         return func.HttpResponse(json.dumps("error : " + error), mimetype="application/json")
